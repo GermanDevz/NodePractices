420ae2ee393f59b8ef817732115ee09f1d04e1db
German Gabriel Velardez <German Velardez>
2022-07-15 19:24:24 -0300

    update models/search.js. Add dotenv

--- models/search.js
+++ models/search.js
@@ -1,8 +1,10 @@
 require('colors')
 
+
+require('dotenv').config();
 const query = 'search/movie'
 
-const api_key ='bbb7be99b28cbabeb49a1afc50e9416e'
+//const api_key ='bbb7be99b28cbabeb49a1afc50e9416e'
  
  const url = 'https://api.themoviedb.org/3/search/movie';
 
@@ -19,14 +21,12 @@ const api_key ='bbb7be99b28cbabeb49a1afc50e9416e'
     }
 
     
-
-
      static getMovies = async(movieName='',lenguage='en-USA')=>{
         const movies =  await axios.create({
             baseURL: `${url}`,
             responseType:'json',
             params:  {
-                'api_key': api_key,
+                'api_key': process.env.TMDB_KEY,
                 'query':movieName,
                 'lenguage':lenguage,
                
@@ -38,7 +38,6 @@ const api_key ='bbb7be99b28cbabeb49a1afc50e9416e'
 
       
      getMovieList = async(movieName='',lenguage='en-USA')=>{
-       
         const moviesList =[];
         const movies =await  Search.getMovies(movieName);
         if(movies.status === 200 ){
@@ -50,21 +49,13 @@ const api_key ='bbb7be99b28cbabeb49a1afc50e9416e'
                 })
             })
         }
-       
-        this.history.push(`${movieName.gray} -> ${(moviesList.length+"").green} results`);
+       this.history.push(`${movieName.gray} -> ${(moviesList.length+"").green} results`);
        return moviesList;
     }
 
 
-
-
-     async  getCity( city = ''){
-        this.history.push(city);
-        return []
-
-    }
     
-    async getHistory(){
+    getHistory = async ()=>{
         console.log(`historial: \n`)
         this.history.forEach((element, index) => {
         console.log(`${index+1} ${element}`);
